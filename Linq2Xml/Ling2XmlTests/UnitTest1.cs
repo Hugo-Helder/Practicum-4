a8d72c7e23586ebb656f1cb7b3860ab24b8b615c
Xander Vedder <xander.vedd@gmail.com>
2021-04-03 14:30:43 +0200

    Restructure practicum 4
    Unsure how I should test this, so we should probably have a chat

---
+++ Linq2Xml/Ling2XmlTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Ling2XmlTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Linq2Xml/Linq2XmlPart1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Linq2XmlPart1
+{
+    public class Track
+    {
+        // ... Implement track
+    }
+
+    public class Cd
+    {
+        // ... Implement Cd
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ... Implement transformation into xml string
+        }
+
+        public static string TransformIntoXmlString(Cd cd)
+        {
+            return "";
+        }
+    }
+}
